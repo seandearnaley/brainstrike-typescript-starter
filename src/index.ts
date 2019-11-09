e43f4a0a0de85b9786003e3c61f4d5a0cf33d06f
Sean Dearnaley <seandearnaley@hotmail.com>
2019-11-08 21:57:40 -0500

    add apollo, eslint, update nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('hello');
+import express = require("express");
+import { ApolloServer, gql } from "apollo-server-express";
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: (): string => "Hello world!"
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);

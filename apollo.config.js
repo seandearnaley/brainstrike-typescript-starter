c1d03d9e7712ad61cc1327ac885571983a0f4b1f
Sean Dearnaley <seandearnaley@hotmail.com>
2019-11-09 03:26:46 -0500

    add apollo client

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'brainstrike',
+      url: 'http://localhost:4000/graphql',
+    },
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Set up our apollo-client to point at the server we created
+// this can be local or a remote endpoint
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      'client-name': 'brainstrike',
+      'client-version': '1.0.0',
+    },
+  }),
+});
+
+// TODO: add Reach router
+
+export const GET_HELLO = gql`
+  query SayHello {
+    hello
+  }
+`;
+
+interface HelloProps {
+  name: string;
+}
+
+const Hello: React.FC<HelloProps> = (props: HelloProps): React.ReactElement => {
+  const { data, loading, error } = useQuery(GET_HELLO);
+
+  if (loading) return <div>loading....</div>;
+  if (error) return <p>ERROR: {error.message}</p>;
+
+  return (
+    <div>
+      {data && data.hello} {props.name}
+    </div>
+  );
+};
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Hello name="sean" />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

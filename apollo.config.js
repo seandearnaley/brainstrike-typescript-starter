79d8e959685b26c4909789391640b8628d6103bc
Sean Dearnaley <seandearnaley@hotmail.com>
2019-11-12 20:36:37 -0500

    move old files

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,11 @@
+// NOTE: schema is generated with apollo graphql code generator
+
 module.exports = {
   client: {
     includes: ['./src/**/*.graphql'],
     service: {
       name: 'brainstrike',
-      url: 'http://localhost:4000/graphql',
+      localSchemaFile: './graphql.schema.json', 
     },
   },
 };
--- src/dash/Dashboard.tsx
+++ src/dash/Dashboard.tsx
@@ -10,26 +10,16 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
-import Chart from './Chart';
-import Deposits from './Deposits';
-import Orders from './Orders';
 
 const Copyright: React.FC = () => {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
+      BrainStrike {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
@@ -125,7 +115,6 @@ const Dashboard: React.FC = () => {
   const handleDrawerClose = (): void => {
     setOpen(false);
   };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
   return (
     <div className={classes.root}>
@@ -182,28 +171,7 @@ const Dashboard: React.FC = () => {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <Orders />
-              </Paper>
-            </Grid>
-          </Grid>
-        </Container>
+
         <Copyright />
       </main>
     </div>

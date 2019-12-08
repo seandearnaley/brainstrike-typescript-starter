abed2828c0fcd800dc100f52d960babb3654e371
Sean Dearnaley <seandearnaley@hotmail.com>
2019-12-07 23:14:49 -0500

    package updates

--- server/src/generated/graphql.ts
+++ server/src/generated/graphql.ts
@@ -20,19 +20,19 @@ export type Scalars = {
    * A date-time string at UTC, such as 2007-12-03T10:15:30Z, compliant with the
    * `date-time` format outlined in section 5.6 of the RFC 3339 profile of the ISO
    * 8601 standard for representation of dates and times using the Gregorian calendar.
-   **/
+   */
   DateTime: any;
   /**
    * A date string, such as 2007-12-03, compliant with the `full-date` format
    * outlined in section 5.6 of the RFC 3339 profile of the ISO 8601 standard for
    * representation of dates and times using the Gregorian calendar.
-   **/
+   */
   Date: any;
   /**
    * A time string at UTC, such as 10:15:30Z, compliant with the `full-time` format
    * outlined in section 5.6 of the RFC 3339profile of the ISO 8601 standard for
    * representation of dates and times using the Gregorian calendar.
-   **/
+   */
   Time: any;
   /** The `Upload` scalar type represents a file upload. */
   Upload: any;

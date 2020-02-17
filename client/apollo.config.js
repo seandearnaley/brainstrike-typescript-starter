c587bb2f5c1db3381668217a8b77099ae5b27e3c
Sean Dearnaley <seandearnaley@hotmail.com>
2020-02-17 02:50:40 -0500

    merge client 3 beta into client

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -1,14 +1,9 @@
-// NOTE: schema is generated with apollo graphql code generator
-
 module.exports = {
   client: {
-    includes: ['./src/graphql/*.graphql'],
+    includes: ['./src/**/*.ts'],
     service: {
       name: 'brainstrike',
       url: 'http://localhost:4000/graphql',
-      // localSchemaFile: './graphql.schema.json', 
     },
   },
 };
-
-
